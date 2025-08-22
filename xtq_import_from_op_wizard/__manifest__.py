8aa344042d0265ad67f30773b23a8c798735fc97
Omar Ruelas <oruelas-prov@trabajo.gob.pe>
2025-08-22 13:37:54 -0500

    [FIX] manifest pipi

--- xtq_import_from_op_wizard/__manifest__.py
+++ xtq_import_from_op_wizard/__manifest__.py
@@ -1,18 +1,20 @@
 ﻿{
     'name': 'XTQ | Asistente para Importar desde OP',
-    'summary': 'Permite a los usuarios crear transferencias de inventario importando componentes desde Órdenes de Producción.',
     'version': '18.0.1.0.1',
     'category': 'Inventory/Inventory',
+    'summary': 'Permite a los usuarios crear transferencias de inventario importando componentes desde Órdenes de Producción.',
     'author': 'XTQ Group',
     'website': 'https://www.xtqgroup.com',
-    'license': 'LGPL-3',
-    'depends': ['stock', 'mrp'],
+    'depends': [
+        'stock',
+        'mrp',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/stock_picking_views.xml',
         'wizards/import_from_op_wizard_views.xml',
     ],
-    'application': False,
     'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
 }
-
