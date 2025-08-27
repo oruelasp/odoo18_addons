738b72892ca34537cda5d43d675efb2c75cea72d
Omar Ruelas <oruelas-prov@trabajo.gob.pe>
2025-08-27 13:17:22 -0500

    [REF] Refactorizar el codigo para crear la subcontratacion

--- mrp_operation_subcontracting/models/__init__.py
+++ mrp_operation_subcontracting/models/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import mrp_workorder
 from . import mrp_workcenter
-# from . import mrp_production
+from . import mrp_workorder
--- mrp_operation_subcontracting/models/mrp_workorder.py
+++ mrp_operation_subcontracting/models/mrp_workorder.py
@@ -6,44 +6,41 @@ from odoo import fields, models, api, Command
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
-    purchase_order_id = fields.Many2one("purchase.order",
-        compute="_compute_purchase_order_id")
+    purchase_order_id = fields.Many2one(
+        "purchase.order",
+        string="Purchase Order",
+        readonly=True,
+        copy=False,
+    )
 
     def _calc_operation_cost(self, opt):
         duration_expected = opt.duration_expected or 0
         return (duration_expected / 60) * opt.workcenter_id.costs_hour
 
-    def _compute_purchase_order_id(self):
-        for workorder in self:
+    def action_create_subcontract_po(self):
+        for workorder in self.filtered(lambda wo: wo.workcenter_id.is_subcontract and not wo.purchase_order_id):
             workcenter = workorder.workcenter_id
-            if not workcenter.is_subcontract:
-                workorder.purchase_order_id = None
-                continue
             name = f"{workorder.id}_{workorder.name}@{workorder.production_id.name}"
             # create service product
-            products = self.env['product.product'].search([('name', '=', name)])
-            if len(products) == 0:
-                product = self.env['product.product'].create([{
+            product = self.env['product.product'].search([('name', '=', name)], limit=1)
+            if not product:
+                product = self.env['product.product'].create({
                     'name': name,
                     'sale_ok': False,
                     'purchase_ok': True,
                     'detailed_type': "service"
-                }])
-            else:
-                product = products[0]
-            po = self.env['purchase.order'].search([('origin', '=', name)], limit=1)
-            if not po:
-                cost = self._calc_operation_cost(workorder)
-                workorder.purchase_order_id = self.env['purchase.order'].create({
-                    'partner_id': workcenter.partner_id.id,  
-                    'state': 'to approve',
-                    'origin': name,
-                    'order_line': [Command.create({
-                        'product_id': product.id,
-                        'product_type': product.detailed_type,
-                        'product_qty': workorder.production_id.product_qty,
-                        'price_unit': cost
-                        }),]
-                }).id
-            else:
-                workorder.purchase_order_id = po.id                
+                })
+
+            cost = self._calc_operation_cost(workorder)
+            po_vals = {
+                'partner_id': workcenter.partner_id.id,
+                'origin': name,
+                'order_line': [Command.create({
+                    'product_id': product.id,
+                    'product_qty': workorder.production_id.product_qty,
+                    'price_unit': cost,
+                })],
+            }
+            purchase_order = self.env['purchase.order'].create(po_vals)
+            workorder.purchase_order_id = purchase_order.id
+        return True                
