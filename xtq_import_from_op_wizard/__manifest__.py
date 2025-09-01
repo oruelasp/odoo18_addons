673b1dcaa1d90a5370960a0672eeba901ba088e0
Omar Ruelas <oruelas-prov@trabajo.gob.pe>
2025-09-01 16:35:36 -0500

    [FIX] Corrige manifest import from wizard

--- xtq_import_from_op_wizard/__manifest__.py
+++ xtq_import_from_op_wizard/__manifest__.py
@@ -1,4 +1,5 @@
-﻿{
+﻿# -*- coding: utf-8 -*-
+{
     'name': 'XTQ | Asistente para Importar desde OP',
     'version': '18.0.1.0.1',
     'category': 'Inventory/Inventory',
@@ -18,4 +19,5 @@
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
+    'auto_install': False,
 }
